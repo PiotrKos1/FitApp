261c485ceb4abf4ebdce7c68214c8702bf1839e9
Piotr Kosinski <piograammer@gmail.com>
2023-05-30 19:30:53 +0200

    Updated

---
+++ App.js
@@ -0,0 +1,9 @@
+import LogInPanel from "./LogInPanel";
+
+function App() {
+    return (<div>
+        <LogInPanel />
+    </div>)
+}
+
+export default App;
---
+++ LogInPanel.js
@@ -0,0 +1,9 @@
+function LogInPanel() {
+    return (
+        <div>
+            loginpanel
+        </div>
+    )
+}
+
+export default LogInPanel;
